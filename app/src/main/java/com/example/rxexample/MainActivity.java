88ff46e4547d716648fa7dbb5675ed701d497dae
aboudmahmoud <aboudmahmoud40@gmail.com>
2022-03-18 19:40:35 +0400

    sevent commit

--- app/src/main/java/com/example/rxexample/MainActivity.java
+++ app/src/main/java/com/example/rxexample/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.rxexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+
+import com.example.rxexample.databinding.ActivityMainBinding;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,152 +30,37 @@ import io.reactivex.rxjava3.subjects.ReplaySubject;
 
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding binding;
+    public static String TAG="aboud";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       //cold  Observable 1 and 2 get all info
-      /*  Observable<Long> cold=Observable.intervalRange(0,5,0,1, TimeUnit.SECONDS);
-        cold.subscribe(aLong ->System.out.println("Student 1  is"+ aLong));
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        cold.subscribe(aLong ->System.out.println("Student 2  is"+ aLong));*/
-        //cold Observable connect to hot  Observable 1 get all info 2 get form the info that send it
-       /* ConnectableObservable<Long> cold=ConnectableObservable.intervalRange(0,5,0,1, TimeUnit.SECONDS).publish();
-        cold.connect();
-        cold.subscribe(aLong ->System.out.println("Student 1  is"+ aLong));
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        cold.subscribe(aLong ->System.out.println("Student 2  is"+ aLong));*/
-        //PublishSubject 1 get all info 2 get form the info that send it
-        /*   PublishSubject<String>subject = PublishSubject.create();
-        subject.subscribe(aLong ->System.out.println("Student 1  is"+ aLong));
-        subject.onNext("A");
-        nape(1000);
-        subject.onNext("B");
-        nape(1000);
-        subject.onNext("C");
-        nape(1000);
-        subject.onNext("D");
-        nape(1000);
-        subject.subscribe(aLong ->System.out.println("Student 2  is "+ aLong));
-        subject.onNext("E");
-        nape(1000);
-        subject.onNext("F");
-        nape(1000);
-        subject.onNext("G");
-        nape(1000);*/
-        //BehaviorSubject 1 get all info 2 get form the last info was  send it
-       /* BehaviorSubject<String>subject = BehaviorSubject.create();
-        subject.subscribe(aLong ->System.out.println("Student 1  is "+ aLong));
-        subject.onNext("A");
-        nape(1000);
-        subject.onNext("B");
-        nape(1000);
-        subject.onNext("C");
-        nape(1000);
-        subject.onNext("D");
-        nape(1000);
-        subject.subscribe(aLong ->System.out.println("Student 2  is "+ aLong));
-        subject.onNext("E");
-        nape(1000);
-        subject.onNext("F");
-        nape(1000);
-        subject.onNext("G");
-        nape(1000);*/
-
-      //  ReplaySubject 1 get all info but when 2 is coming 1 stop geting info tell 2 get all perivos info then both get the remainning info
-     /*   ReplaySubject<String>subject = ReplaySubject.create();
-        subject.subscribe(aLong ->System.out.println("Student 1  is "+ aLong));
-        subject.onNext("A");
-        nape(1000);
-        subject.onNext("B");
-        nape(1000);
-        subject.onNext("C");
-        nape(1000);
-        subject.onNext("D");
-        nape(1000);
-        subject.subscribe(aLong ->System.out.println("Student 2  is "+ aLong));
-        subject.onNext("E");
-        nape(1000);
-        subject.onNext("F");
-        nape(1000);
-        subject.onNext("G");
-        nape(1000);*/
-  //AsyncSubject both 1 an  2 getall info just last one
-       /* AsyncSubject<String> subject = AsyncSubject.create();
-        subject.subscribe(aLong ->System.out.println("Student 1  is "+ aLong));
-        subject.onNext("A");
-        nape(1000);
-        subject.onNext("B");
-        nape(1000);
-        subject.onNext("C");
-        nape(1000);
-        subject.onNext("D");
-        nape(1000);
-        subject.subscribe(aLong ->System.out.println("Student 2  is "+ aLong));
-        subject.onNext("E");
-        nape(1000);
-        subject.onNext("F");
-        nape(1000);
-        subject.onNext("G");
-        nape(1000);
-        subject.onComplete();*/
-        //observable.create to observer using suscribe pathing observer as pramter and send data using onnext
-        /*Observable observable=Observable.create(new ObservableOnSubscribe<Object>() {
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Observable.create(new ObservableOnSubscribe<Object>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Object> emitter) throws Throwable {
-                for (int i=0 ; i<5 ;i++)
-                {
-                    emitter.onNext("Aboud is Codeing "+ i);
-                }
-                emitter.onComplete();
+                binding.text.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                        emitter.onNext(charSequence);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
             }
-        });*/
-
-        //Observable.just to observer with pathing item limit 10 only with auto complet
-        /*Observable observable=Observable.just(0,1,2,3,5);*/
-
-        //Observable.fromArray take array as parrmeter
-        /* Integer[] list = new Integer[5];
-        for (int i=0 ; i<5; i++)
-        {
-            list[i]=i;
-        }
-        Observable observable=Observable.fromArray(list);*/
-
-        //Observable.range without peroud in between
-       // Observable observable=Observable.range(0,5);
-
-
-        Observable.just(1,2,3,4,5)
-                .subscribeOn(Schedulers.io())
-                //Scend subscribeOn Will not Work
-                .subscribeOn(Schedulers.newThread())
-                .doOnNext(c -> Log.d("aboud","Upstream " + c + " Cureent Thread"+Thread.currentThread().getName()))
-                .observeOn(Schedulers.computation())
-                //Scend .observeOn or the last one will work
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(o -> Log.d("aboud","DownStream " + o + " Cureent Thread"+Thread.currentThread().getName()));
-        nape(300);
-
-
-
+        })
+                .doOnNext(o -> Log.d(TAG,"UpsTream:"+o))
+                .subscribe(o -> Log.d(TAG, "DownStream:"+o));
     }
 
-    public void nape(int i)
-    {
-        try {
-            Thread.sleep(i);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
